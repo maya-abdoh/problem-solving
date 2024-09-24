d4987b1a8ee224c02e42afc7059c9d8d541ac188
maya-abdoh <92648274+maya-abdoh@users.noreply.github.com>
2024-09-24 22:06:13 +0300

    Time: 3 ms (97.98%) | Memory: 13.1 MB (72.06%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
-        if (numRows == 1 || s.size() <= numRows) return s;
+    string convert(string text, int totalRows) {
+        if (totalRows == 1 || text.size() <= totalRows) return text;
 
-        vector<string> rows(numRows);  
-        int curRow = 0;   
-        int direction = -1;  
+        vector<string> zigzagRows(totalRows);  
+        int currentRow = 0;   
+        int rowDirection = -1; 
 
-        for (int i = 0; i < s.size(); ++i) {
-            rows[curRow] += s[i]; 
+        for (int i = 0; i < text.size(); ++i) {
+            zigzagRows[currentRow] += text[i];   
 
-            if (curRow == 0 || curRow == numRows - 1) {
-                direction = -direction;
+            if (currentRow == 0 || currentRow == totalRows - 1) {
+                rowDirection = -rowDirection;
             }
 
-            curRow += direction;
+            currentRow += rowDirection;
         }
         
-        string result;
-        for (int i = 0; i < rows.size(); ++i) {
-            result += rows[i];
+        string finalResult;
+        for (int i = 0; i < zigzagRows.size(); ++i) {
+            finalResult += zigzagRows[i];
         }
-        return result;
+        return finalResult;
     }
 };
